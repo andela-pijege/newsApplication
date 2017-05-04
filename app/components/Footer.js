53400a326f2db31001b664bedf5389990bec00a1
Andela Developer <andeladeveloper@Preciouss-MacBook-Pro.local>
2017-05-04 18:11:44 +0100

    setup testing environment and a little modification of the UI

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -8,22 +8,23 @@ class Footer extends React.Component {
           <div className="container">
             <div className="row">
               <div className="col l6 s12">
-                <h5 className="white-text">Footer Content</h5>
-                <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <h5 className="white-text">Nwes 24/7</h5>
+                <p className="grey-text text-lighten-4">bringing you the best news worldwide</p>
               </div>
               <div className="col l4 offset-l2 s12">
-                <h5 className="white-text">Links</h5>
+                <h6 className="white-text">follw us</h6>
                 <ul>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a className="grey-text text-lighten-3" href="https://www.facebook.com/news24/7" rel="noopener noreferrer" target="_blank">facebook</a></li>
+                  <li><a className="grey-text text-lighten-3" href="https://www.twitter.com/news24/7" rel="noopener noreferrer" target="_blank">twitter</a></li>
+                  <li><a className="grey-text text-lighten-3" href="https://www.instagram.com/news24/7" rel="noopener noreferrer" target="_blank">instagram</a></li>
                 </ul>
               </div>
             </div>
           </div>
           <div className="footer-copyright">
             <div className="container">
-            © 2017 Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+            © 2017 Copyright News 24/7
+            <a className="grey-text text-lighten-4 right" href="#!">News</a>
             </div>
           </div>
         </footer>
--- app/components/Nav.js
+++ app/components/Nav.js
@@ -12,7 +12,7 @@ class Nav extends Component {
       <div className="navbar-fixed">
         <nav className="black">
           <div className="nav-wrapper container">
-            <a href="#" className="brand-logo">Logo</a>
+            <a href="#" className="brand-logo">News 24/7</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
               <li><a href="/">News</a></li>
               <li>logout</li>
