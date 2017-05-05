4b2cb52968729ee32567f66bd719f69e83198ca2
Andela Developer <andeladeveloper@Preciouss-MacBook-Pro.local>
2017-05-05 09:57:35 +0100

    add procfile and fix heroku bugs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 app.use((req, res, next) => {
   if (req.headers['x-forwarded-proto'] === 'https') {
