8671ffffd82d837e85feac6184f37378b3964dfe
Andela Developer <andeladeveloper@Preciouss-MacBook-Pro.local>
2017-05-08 13:20:06 +0100

    wrote a few test to integrate coveralls

--- app/tests/components/NavTest.js
+++ app/tests/components/NavTest.js
@@ -8,4 +8,8 @@ describe('Test Nav component', () => {
   it('should contain a root node of div', () => {
     expect(wrapper.type()).toBe('div');
   });
+  it('should contain a root container with a class name', () => {
+    // console.log(wrapper.node.props.className);
+    expect(wrapper.node.props.className).toBe('navbar-fixed');
+  });
 });
